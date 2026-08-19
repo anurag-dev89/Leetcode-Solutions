b05d8d252e8efc6696a5b6407f5d1a6884317782
Anurag Patel <anuragpatel212121@gmail.com>
2026-08-19 23:20:10 +0530

    Time: 3 ms (86.09%), Space: 43.1 MB (86.14%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,24 @@
+class Solution {
+public static boolean isValid(String s) {
+
+        Stack <Character> mystack = new Stack();
+
+        for(int i=0; i<s.length();i++){
+
+        char ch= s.charAt(i);
+
+          if(ch == '('){
+            mystack.push(')');   
+          }else if(ch == '['){
+            mystack.push(']');
+          }else if(ch == '{'){
+            mystack.push('}');
+          }else{
+           if( mystack.isEmpty() || mystack.pop()!= ch){
+            return false;
+           }
+          }
+        }
+        return mystack.isEmpty();
+    }
+}
